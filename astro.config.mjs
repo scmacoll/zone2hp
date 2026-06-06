81aba6c3eb9c0aa1c5b4b48723ed1122f80789a8
scmacoll <85879687+scmacoll@users.noreply.github.com>
2026-06-06 21:14:03 +1000

    changed a2 to index.html. a2 official home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zone2hp.com',
+  site: "https://zone2hp.com",
+  devToolbar: { enabled: false },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
